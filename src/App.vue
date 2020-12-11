75db8d23be1f681a5e1ec3dca5983cdc79fa5657
mouhsineelachbi <mouhsineelachbi@gmail.com>
2020-12-11 21:50:44 +0100

    scoped and global style

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>My first Vue app</h1>
+  <Modal />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Modal from './components/Modal.vue';
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Modal
   }
 };
 </script>
@@ -22,5 +22,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  color: yellow;
 }
 </style>
